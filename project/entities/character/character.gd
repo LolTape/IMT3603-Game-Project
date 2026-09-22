285fa351ef6912171f456383b27bb410c44fbb73
Lucas Jan Kloos <lucasjk@stud.ntnu.no>
2026-09-22 12:48:14 +0200

    feature(movement): made variables more intuitive and made seperate jump and fall gravity
    Co-authored-by: Andreas Castro Messel <andrecme@stud.ntnu.no>

--- project/entities/character/character.gd
+++ project/entities/character/character.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
 @export var acceleration = 500
-@export var gravity = 50
-@export var jump_height : float = 2
 var horizontalDrag = 0.7
-# @export var jump_time_to_peak : float
-# @export var jump_time_to_descent : float
 
-@onready var jump_speed = -sqrt(jump_height * 1000000)
+@export var jump_height : float
+@export var jump_time_to_peak : float
+@export var jump_time_to_descent : float
+
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((2.0 * jump_height) / pow(jump_time_to_peak, 2.0)) * 1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,24 +20,23 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _physics_process(delta: float):
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump()
+
+	apply_input()
+	velocity += Vector2.DOWN * gravity()
+	velocity.x *= horizontalDrag
+
+	move_and_slide()
+	
 func jump():
-	var on_ground = is_on_floor()
-	if on_ground:
-		velocity.y += jump_speed
+	velocity.y += jump_velocity
 
 func apply_input():
 	var direction = Input.get_axis("left", "right")
 	velocity.x += direction * acceleration 
 
-	if Input.is_action_just_pressed("jump"):
-		jump()
-
-
-func _physics_process(delta: float):
-	apply_input()
-	velocity += Vector2.DOWN * gravity
-	velocity.x *= horizontalDrag
-
-	move_and_slide()
-	
-	
+func gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
