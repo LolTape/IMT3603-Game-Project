b3ee1707dae70708b11149726d79ca38be0dd96a
Lucas Jan Kloos <lucasjk@stud.ntnu.no>
2026-09-22 13:28:43 +0200

    feat(movement): reworked horizontal movement script
    Co-authored-by: Andreas Castro Messel <andrecme@stud.ntnu.no>

--- project/entities/character/character.gd
+++ project/entities/character/character.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@export var acceleration = 500
-var horizontalDrag = 0.7
+@export var speed = 1500
 
 @export var jump_height : float
 @export var jump_time_to_peak : float
 @export var jump_time_to_descent : float
+@export var fall_speed : float
 
 @onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
 @onready var jump_gravity : float = ((2.0 * jump_height) / pow(jump_time_to_peak, 2.0)) * 1.0
@@ -20,23 +20,28 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func jump():
+	velocity.y += jump_velocity
+
+func gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+
+
+
 func _physics_process(delta: float):
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump()
+	if !Input.is_action_pressed("jump") and !is_on_floor():
+		velocity.y += fall_gravity * delta
 
-	apply_input()
-	velocity += Vector2.DOWN * gravity()
-	velocity.x *= horizontalDrag
+	var direction = Input.get_axis("left", "right")
+	var target_velocity = direction * speed
+	velocity.x = move_toward(velocity.x, target_velocity, speed/5.0)
+
+	velocity.y += gravity() * delta
+	# velocity.y = clamp(velocity.y, INT64_MIN, fall_speed)
 
 	move_and_slide()
 	
-func jump():
-	velocity.y += jump_velocity
-
-func apply_input():
-	var direction = Input.get_axis("left", "right")
-	velocity.x += direction * acceleration 
-
-func gravity() -> float:
-	return jump_gravity if velocity.y < 0.0 else fall_gravity
